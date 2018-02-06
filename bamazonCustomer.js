b555e22f9112b2b6a81378e02ed6e1a38c5e736a
eggobiggie <miacrandell@gmail.com>
2018-02-06 13:24:44 -0700

    add connection from js file to SQL, add function to display all product information from db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,33 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected as id ' + connection.threadId);
+});
+
+//Function to display the product information from mySQL maybe with a message
+
+function displayAll () {
+    connection.query(
+        'SELECT * FROM `products`',
+        function(err, results, fields) {
+            console.log("\nCurrent Products Availabe: \n");
+            for (i = 0; i < results.length; i++) {
+                console.log("Product ID: " + results[i].id + " | Product: " + results[i].product_name + " | Department: " + results[i].department_name + " | Price: $" + results[i].price); // results contains rows returned by server
+            }
+        }
+    );
+}
+
+displayAll();
+   
+
